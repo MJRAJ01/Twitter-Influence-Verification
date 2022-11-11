8728ed0fe59315c6719c686b5c6e8ba0eb153c49
jshultz00 <justin.shultz@vanderbilt.edu>
2022-11-10 22:20:31 -0600

    mini network centered around elon musk

--- TwitterScrape.py
+++ TwitterScrape.py
@@ -9,19 +9,21 @@ client = tweepy.Client(bearer_token=bearer_token,
                        access_token_secret=access_token_secret,
                        wait_on_rate_limit=True)
 
-followers = {}
+network = {}
+visited = {}
 elon = client.get_user(id=44196397, user_fields='public_metrics')
 elon_username = elon.data.username
-elon_follower_count = elon.data.public_metrics["followers_count"]
+elon_follower_count = elon.data.public_metrics['followers_count']
 elon_id = elon.data.id
+elon_followers = []
+network[elon_id] = (elon_username, elon_follower_count, elon_followers)
+print(network)
 follower_names = client.get_users_followers(id=44196397, user_fields='public_metrics', max_results=10)
+visited[elon_id] = True
 for user in follower_names.data:
-
-followers[elon_username] = (elon_follower_count, )
-print(f'username: {elon_username}, id: {elon_id}, followers: {elon_follower_count}')
-
-#
-# for user in followers.data:
+    network[elon_id][2].append(user.id)
+    network[user.id] = (user.username, user.public_metrics['followers_count'], [])
+print(network)
 #     print(user.id)
 # print(f"Follower Count: {elon}")
 # likes = {}
