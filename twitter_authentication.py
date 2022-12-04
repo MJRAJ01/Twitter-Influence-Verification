dd065a8f0e5ac8458033b1aa3c9042036ce1c163
Mark Raj <61257131+MJRAJ01@users.noreply.github.com>
2022-12-04 16:07:20 -0600

    reorganization

--- twitter_authentication.py
+++
@@ -1,6 +0,0 @@
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAEdbjAEAAAAARzRnLihwQ6jRC%2FDXOcsxF%2BPkUbE" \
-               "%3DFTsSVZERBrShHMqcGpcO65IxzDOFapZVqUMb4IRy1BC6ZjcmhZ"
-consumer_key = "1JA3ND1qukr6Lo5ygHowhYbyq"
-consumer_secret = "Zi0jcV1gChLofOBr4JWgmcWBVSvpz4UyvQKEfc4IJs0TYb9CZq"
-access_token = "1590065742807826432-AX1Fun3mawTwY65Z1yxOPCcX3olY04"
-access_token_secret = "n8DROuUaz3YBdPpHBgbQUrL8xqysUH97OkuQ0JiTkJF1P"
