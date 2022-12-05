a032db96edec76b5ba83a6b7df68340b56ac6aff
jshultz00 <justin.shultz@vanderbilt.edu>
2022-12-05 17:45:25 -0600

    adjusted fake network

--- fakeNetwork.py
+++ fakeNetwork.py
@@ -49,49 +49,60 @@ from matplotlib import pyplot as plt
 #     print(f"Count: {count}")
 #     return real_attrs
 
-network = nx.Graph()
-attrs = {}
-for i in range(1000):
-    rand1 = random.randint(10, 1000)
-    rand2 = random.randint(1000, 10000)
-    rand3 = random.randint(10000, 50000)
-    rand4 = random.randint(50000, 1000000)
-    p_followers = .95
-    if random.random() < p_followers:
+def fake_network():
+    network = nx.Graph()
+    attrs = {}
+    for i in range(1000):
+        rand1 = random.randint(10, 1000)
+        rand2 = random.randint(1000, 10000)
+        rand3 = random.randint(10000, 50000)
+        rand4 = random.randint(50000, 1000000)
+        p_followers = .95
         if random.random() < p_followers:
             if random.random() < p_followers:
-                attrs[i] = {"id": i, "followers": rand1, "color": "green"}
-                network.add_node(i)
+                if random.random() < p_followers:
+                    attrs[i] = {"id": i, "followers": rand1, "color": "green"}
+                    network.add_node(i)
+                else:
+                    attrs[i] = {"id": i, "followers": rand2, "color": "yellow"}
+                    network.add_node(i)
             else:
-                attrs[i] = {"id": i, "followers": rand2, "color": "yellow"}
+                attrs[i] = {"id": i, "followers": rand3, "color": "orange"}
                 network.add_node(i)
         else:
-            attrs[i] = {"id": i, "followers": rand3, "color": "orange"}
+            attrs[i] = {"id": i, "followers": rand4, "color": "red"}
             network.add_node(i)
-    else:
-        attrs[i] = {"id": i, "followers": rand4, "color": "red"}
-        network.add_node(i)
-    if random.random() < 0.998:
-        attrs[i]["verified"] = False
-    else:
-        attrs[i]["verified"] = True
-nx.set_node_attributes(network, attrs)
-p_edge = 0.1
-for i in range(network.number_of_nodes()):
-    for j in range(i + 1, network.number_of_nodes()):
-        if network.nodes[i]["followers"] >= 50000 or network.nodes[i]["followers"] >= 50000:
-            p_edge = 0.25
-        elif network.nodes[i]["followers"] >= 10000 or network.nodes[i]["followers"] >= 10000:
-            p_edge = 0.1
-        elif network.nodes[i]["followers"] >= 1000 or network.nodes[i]["followers"] >= 1000:
-            p_edge = 0.05
+        if attrs[i]["followers"] < 42000:
+            if random.random() < 0.998:
+                attrs[i]["verified"] = False
+            else:
+                attrs[i]["verified"] = True
         else:
-            p_edge = 0.025
+            if random.random() < 0.50:
+                attrs[i]["verified"] = False
+            else:
+                attrs[i]["verified"] = True
+    nx.set_node_attributes(network, attrs)
+    p_edge = 0.1
+    for i in range(network.number_of_nodes()):
+        for j in range(i + 1, network.number_of_nodes()):
+            if network.nodes[i]["followers"] >= 50000 or network.nodes[i]["followers"] >= 50000:
+                p_edge = 0.25
+            elif network.nodes[i]["followers"] >= 10000 or network.nodes[i]["followers"] >= 10000:
+                p_edge = 0.1
+            elif network.nodes[i]["followers"] >= 1000 or network.nodes[i]["followers"] >= 1000:
+                p_edge = 0.05
+            else:
+                p_edge = 0.025
 
-        if random.random() < p_edge \
-                and len(network.edges(i)) < network.nodes[i]["followers"] \
-                and len(network.edges(j)) < network.nodes[j]["followers"]:
-            network.add_edge(i, j)
+            if random.random() < p_edge \
+                    and len(network.edges(i)) < network.nodes[i]["followers"] \
+                    and len(network.edges(j)) < network.nodes[j]["followers"]:
+                network.add_edge(i, j)
+    return network
+
+
+network = fake_network()
 node_sizes = []
 colors = []
 # if we switch to directed graph we should use katz or pagerank
@@ -104,4 +115,4 @@ plt.show()
 
 # real = nx.Graph()
 # real_attrs = readFromFile("hedden_network_with_followers_verified.csv", real)
-print()
+nx.write_gexf(network, 'fakeNetwork.gexf')
